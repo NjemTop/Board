4b57c3cc8a149542a576f11bc4476f299d57f00e
Олег <106114697+NjemTop@users.noreply.github.com>
2023-02-28 01:08:06 +0300

    + коммент

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def start_socket_server():
 # запуск двух функций (запуск скрипта ВЭБ-СЕРВЕРА и запуск скрипта телебота)
 if __name__ == '__main__':
     p1 = multiprocessing.Process(target=start_socket_server)
+    # Здесь мы используем threading.Thread для запуска функции start_telegram_bot в отдельном потоке, который не блокирует выполнение остального кода.
     p2 = threading.Thread(target=start_telegram_bot)
     p1.start()
     p2.start()
