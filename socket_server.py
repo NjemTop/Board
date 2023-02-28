f57ef06bfe801d64d348eebc7fdc20ec3cec4645
Олег <106114697+NjemTop@users.noreply.github.com>
2023-02-28 14:26:19 +0300

    + в папку лог

--- socket_server.py
+++ socket_server.py
@@ -9,7 +9,7 @@ from telegram_bot import send_telegram_message
 error_logger = logging.getLogger('error_logger')
 error_logger.setLevel(logging.ERROR)
 # создаем обработчик, который будет записывать ошибки в файл Web-error.log
-error_handler = logging.FileHandler('Web-errors.log')
+error_handler = logging.FileHandler('logs/Web-errors.log')
 error_handler.setLevel(logging.ERROR)
 # создаем форматирование
 error_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M'))
@@ -19,7 +19,7 @@ error_logger.addHandler(error_handler)
 # Создание объекта логгера для информационных сообщений
 info_logger = logging.getLogger('info_logger')
 info_logger.setLevel(logging.INFO)
-info_handler = logging.FileHandler('web-info.log')
+info_handler = logging.FileHandler('logs/web-info.log')
 info_handler.setLevel(logging.INFO)
 info_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M'))
 info_logger.addHandler(info_handler)
@@ -54,6 +54,8 @@ while True:
         # Принимаем данные порциями и ретранслируем их
         while True:
             data = connection.recv(8192)
+            print(f'Пришли данные от адреса: {client_address}')
+            info_logger.info('Пришли данные от адреса: %s', client_address)
             if not data:
                 print('Нет данных от:', client_address)
                 info_logger.info('Нет данных от: %s', client_address)
