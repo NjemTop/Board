fd910a56ed932370b13e49039c329aadc76bd1e4
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-01 00:49:00 +0300

    Add files via upload

--- web_server.py
+++ web_server.py
@@ -28,8 +28,11 @@ def get_app():
     @app.route('/', methods=['POST'])
     def handle_ticket():
         """Функция обработки вэбхуков из HappyFox"""
-        ip_address = (f"Request from {request.remote_addr}: {request.url}")
+        ip_address = f"Request from {request.remote_addr}: {request.url}"
+        user_agent = request.headers.get('User-Agent')
+        user_who = f'User-Agent: {user_agent}'
         info_logger.info('Кто-то зашёл на сайт c IP-адреса: %s', ip_address)
+        info_logger.info('Его данные подключения: %s', (user_who,))
         return Response('Чё пришёл сюда?', mimetype='text/plain')
     
     @app.route('/create_ticket', methods=['POST'])
