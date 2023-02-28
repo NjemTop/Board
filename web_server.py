6bebb66e4dc4ac432d615620338f1e82f82d2891
Олег <106114697+NjemTop@users.noreply.github.com>
2023-02-28 18:54:20 +0300

    вэб сервер на flask

---
+++ web_server.py
@@ -0,0 +1,70 @@
+﻿"""Модуль парсинга джейсона и flask, через которое работает ВЭБ-приложение"""
+import json
+from flask import Flask, request
+import logging
+from telegram_bot import send_telegram_message
+
+# Создание объекта логгера для ошибок и критических событий
+error_logger = logging.getLogger('error_logger')
+error_logger.setLevel(logging.ERROR)
+error_handler = logging.FileHandler('web-errors.log')
+error_handler.setLevel(logging.ERROR)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M')
+error_handler.setFormatter(formatter)
+error_logger.addHandler(error_handler)
+
+# Создание объекта логгера для информационных сообщений
+info_logger = logging.getLogger('info_logger')
+info_logger.setLevel(logging.INFO)
+info_handler = logging.FileHandler('web-info.log')
+info_handler.setLevel(logging.INFO)
+error_handler.setFormatter(formatter)
+info_logger.addHandler(info_handler)
+
+app = Flask(__name__)
+
+# Чат айди, куда отправляем алерты
+alert_chat_id = -1001760725213
+
+@app.route('/', methods=['POST'])
+def handle_ticket():
+    """Сервер принятий вэбхуков из HappyFox"""
+    message = ""
+    message = request.data.decode('utf-8')
+    try:
+        # находим JSON в сообщении
+        json_start = message.find('{')
+        if json_start != -1:
+            json_str = message[json_start:]
+            print('--'*60)
+            # парсим JSON
+            json_data = json.loads(json_str)
+            # находим значения для ключей
+            ticket_id = json_data.get("ticket_id")
+            subject = json_data.get("subject")
+            priority_name = json_data.get("priority_name")
+            agent_ticket_url = json_data.get("agent_ticket_url")
+            print('**'*60)
+            # отправляем сообщение в телеграм-бот
+            ticket_message = (f"Новый тикет: {ticket_id}\nТема: {subject}\nПриоритет: {priority_name}\nСсылка: {agent_ticket_url}")
+            print(ticket_message)
+            send_telegram_message(alert_chat_id, ticket_message)
+            info_logger.info('Отправлена следующая информация в группу: %s', ticket_message)
+        else:
+            print('JSON не найден в сообщении.')
+            error_logger.error("JSON не найден в сообщении. %s")
+    except json.decoder.JSONDecodeError:
+        print('Не удалось распарсить JSON в запросе.')
+        error_logger.error("Не удалось распарсить JSON в запросе. %s")
+        return 'Не удалось распарсить JSON в запросе.', 400
+
+    return "OK", 200
+
+if __name__ == '__main__':
+    try:
+        server_address = ('0.0.0.0', 3030)
+        app.run(host=server_address[0], port=server_address[1], debug=True)
+        info_logger.info('Сервер запущен на порту %s', server_address[1])
+    except Exception as e:
+        error_logger.error("Ошибка при запуске ВЭБ-сервера: %s", e)
+        raise e
