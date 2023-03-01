1ba958d464de9e5f6f5fbecf8464ef31b013077c
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-01 13:03:15 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -696,7 +696,5 @@ def send_text_for_stat_update_SB(result_SB_update_statistic):
         button_update_statistics_SB.add(back_from_result_SB_update_statistic, main_menu, row_width=2)
         bot.send_message(result_SB_update_statistic.from_user.id, text='Запрос не соответствует условиям. Пожалуйста, вернитесь назад и повторите попытку.', reply_markup=button_update_statistics_SB) 
    
-def start_telegram_bot():
-    """"Функция запуска телебота"""
-    # запуск бота
-    bot.infinity_polling()
+
+  bot.infinity_polling()
