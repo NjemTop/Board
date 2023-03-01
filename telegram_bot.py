a492975baec3a9694722917b6b6da8fb4c98ab66
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-01 07:40:22 +0300

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -156,6 +156,14 @@ def send_verification_code(email_access):
                 bot.register_next_step_handler(password_message, check_pass_answer)
                 # Ищем полученную почту в системе HappyFox
                 try:
+                    ### Авторизация в HappyFox
+                    # извлекаем значения API_KEY и API_SECRET
+                    API_KEY = DATA['HAPPYFOX_SETTINGS']['API_KEY']
+                    API_SECRET = DATA['HAPPYFOX_SETTINGS']['API_SECRET']
+                    # сохраняем значения в переменную auth
+                    auth = (API_KEY, API_SECRET)
+                    API_ENDPOINT = DATA['HAPPYFOX_SETTINGS']['API_ENDPOINT']
+                    headers = {'Content-Type': 'application/json'}
                     staff = requests.get(API_ENDPOINT + '/staff/', auth=auth, headers=headers).json()
                     for staff_member in staff:
                         if staff_member['email'] == email_access.text:
