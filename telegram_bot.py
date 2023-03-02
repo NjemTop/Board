78a858f69c4ea8a743431e8042aa6c0fb9c1563e
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-02 15:19:26 +0300

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -170,7 +170,7 @@ def send_verification_code(email_access):
                 server.sendmail(EMAIL_FROM, dest_email, msg.as_string())
                 ## Бот выдает сообщение с просьбой ввести пароль + вносим почту пользователя в БД
                 password_message = bot.send_message(email_access.chat.id, "Пожалуйста, введите пароль, отправленный на указанную почту.")
-                bot.register_next_step_handler(password_message, check_pass_answer, access_password)
+                bot.register_next_step_handler(password_message, access_password, check_pass_answer)
                 # Ищем полученную почту в системе HappyFox
                 try:
                     # Делаем переменные глобальные, чтобы передать их в функцию check_pass_answer
