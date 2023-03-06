398220dd56966fb21e5e0cea10fd5aaf350f11b7
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-06 22:14:26 +0300

    Add files via upload

--- web_server.py
+++ web_server.py
@@ -78,18 +78,18 @@ def handle_client_reply(json_data):
             if alert_chat_id is None:
                 web_error_logger.error("Не удалось найти 'chat_id' для пользователя %s", assignee_name)
                 # Отправляем ответ о том, что приняли файлы, однако не нашли полезной информации, но приняли же (200)
-                return "OK"
+                return "OK", None
         except FileNotFoundError as error_message:
             web_error_logger.error("Не удалось найти файл data.xml. Ошибка: %s", error_message)
-            return 'Не удалось найти файл data.xml.'
+            return None, 'Не удалось найти файл data.xml.'
         # Отправляем сообщение в телеграм-бот
         send_telegram_message(alert_chat_id, ticket_message)
         web_info_logger.info('В чат: %s, направлена информация о новом сообщении в тикете: %s', assignee_name, ticket_id)
         # Отправляем ответ о том, что всё принято и всё хорошо (201)
-        return "OK"
+        return "OK", None
     except ValueError as error_message:
         web_error_logger.error("Не удалось собрать инфорамацию из запроса, который прислал HappyFox %s", error_message)
-        return 'Не удалось собрать инфорамацию из запроса, который прислал HappyFox.'
+        return None, 'Не удалось собрать инфорамацию из запроса, который прислал HappyFox.'
 
 def handle_assignee_change(json_data):
     """Обработка изменения назначенного в тикете"""
@@ -105,26 +105,30 @@ def handle_assignee_change(json_data):
         if who_change == new_assignee_name:
             web_info_logger.info('Сотрудник: %s, сам себе назначил тикет %s, поэтому алерт не нужен', who_change, ticket_id)
             # Отправляем ответ о том, что приняли файлы, однако не нашли полезной информации, но приняли же (200)
-            return "OK"
+            return "OK", None
         # Формируем сообщение в текст отправки
         new_assignee_name_message = (f"Сотрудником: {who_change}\nИзменил назначенного в тикете: {ticket_id}\nТема: {subject}\nИмя клиента: {client_name}\nПриоритет: {priority_name}\nСсылка: {agent_ticket_url}")
-        # Находим все элементы header_footer внутри элемента user
-        header_footer_elements = ET.parse('data.xml').getroot().findall('.//user/header_footer')
-        # Задаем начальное значение alert_chat_id
-        alert_chat_id = get_alert_chat_id(header_footer_elements, new_assignee_name)
-        # Если alert_chat_id не был найден, выводим ошибку
-        if alert_chat_id is None:
-            web_error_logger.error("Не удалось найти 'chat_id' для пользователя %s", new_assignee_name)
-            # Отправляем ответ о том, что приняли файлы, однако не нашли полезной информации, но приняли же (200)
-            return "OK"
-        # Отправляем сообщение в телеграм-бот
-        send_telegram_message(alert_chat_id, new_assignee_name_message)
-        web_info_logger.info('В чат %s, отправлена информация об изменении отвественного, номер тикета: %s', new_assignee_name, ticket_id)
-        # Отправляем ответ о том, что всё принято и всё хорошо (201)
-        return "OK"
+        try:
+            # Находим все элементы header_footer внутри элемента user
+            header_footer_elements = ET.parse('data.xml').getroot().findall('.//user/header_footer')
+            # Задаем начальное значение alert_chat_id
+            alert_chat_id = get_alert_chat_id(header_footer_elements, new_assignee_name)
+            # Если alert_chat_id не был найден, выводим ошибку
+            if alert_chat_id is None:
+                web_error_logger.error("Не удалось найти 'chat_id' для пользователя %s", new_assignee_name)
+                # Отправляем ответ о том, что приняли файлы, однако не нашли полезной информации, но приняли же (200)
+                return "OK", None
+            # Отправляем сообщение в телеграм-бот
+            send_telegram_message(alert_chat_id, new_assignee_name_message)
+            web_info_logger.info('В чат %s, отправлена информация об изменении отвественного, номер тикета: %s', new_assignee_name, ticket_id)
+            # Отправляем ответ о том, что всё принято и всё хорошо (201)
+            return "OK", None
+        except FileNotFoundError as error_message:
+            web_error_logger.error("Не удалось найти файл data.xml. Ошибка: %s", error_message)
+            return None, 'Не удалось найти файл data.xml.'
     except ValueError as error_message:
         web_error_logger.error("Не удалось собрать инфорамацию из запроса, который прислал HappyFox %s", error_message)
-        return 'Не удалось собрать инфорамацию из запроса, который прислал HappyFox.'
+        return None, 'Не удалось собрать инфорамацию из запроса, который прислал HappyFox.'
 
 def get_app():
     """Функция приложения ВЭБ-сервера"""
