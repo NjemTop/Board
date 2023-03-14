88a00df89f0f0ce5d4dd99f3f50f9a7b79bf8a5c
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-14 17:34:20 +0300

    Add files via upload

--- check_tickets.py
+++ check_tickets.py
@@ -3,8 +3,11 @@ import json
 import logging
 import datetime
 from datetime import datetime, timedelta
-import pytz
+import schedule
+import time
+import datetime
 import xml.etree.ElementTree as ET
+from telegram_bot import send_telegram_message
 
 # Создание объекта логгера для ошибок и критических событий
 check_error_logger = logging.getLogger('Check_Ticket_Error')
@@ -38,12 +41,57 @@ HEADERS = {'Content-Type': 'application/json'}
 
 def get_time_diff(date_str):
     """Функция подсчёта времени с последнего ответа клиента"""
-    current_time = datetime.now()
-    date_time = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
-    # прибавляем 3 часа
-    date_time = date_time + timedelta(hours=3)
-    diff = current_time - date_time
+    current_time = datetime.datetime.now()
+    date_time = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=3)
+
+    # Определяем день недели
+    day_of_week = date_time.weekday()
+
+    # Смещаем дату назад на количество дней, равное количеству выходных
+    days_to_subtract = 0
+    if day_of_week == 5:  # суббота
+        days_to_subtract = 1
+    elif day_of_week == 6:  # воскресенье
+        days_to_subtract = 2
+
+    # Вычисляем время между датами, исключая выходные
+    business_days = 0
+    elapsed_days = (current_time - date_time).days
+    for day in range(1, elapsed_days+1):
+        d = date_time + datetime.timedelta(days=day)
+        if d.weekday() not in (5, 6):
+            business_days += 1
+    elapsed_days = business_days - days_to_subtract
+    diff = datetime.timedelta(days=elapsed_days, seconds=(current_time - date_time).seconds)
+
     return diff
+
+def get_assigned_name(assigned_to):
+    if isinstance(assigned_to, list):
+        for assigned in assigned_to:
+            assigned_name = assigned.get('name')
+            break
+    else:
+        if assigned_to is not None:
+            assigned_name = assigned_to.get('name')
+        else:
+            assigned_name = "Нет исполнителя"
+    return assigned_name
+
+def get_name_info(contact_info):
+    name_info = None
+    for contact in contact_info:
+        name_info = contact.get('name')
+        break
+    return name_info
+
+def get_contact_name(contact_groups):
+    """Функция для получения имени контактной группы"""
+    name_info = None
+    for contact in contact_groups:
+        name_info = contact.get('name')
+        break
+    return name_info
 
 def get_alert_chat_id(assigned_name):
     """"Функция получения chat_id для отправки сообщения"""
@@ -64,16 +112,51 @@ def get_alert_chat_id(assigned_name):
 
     # Если alert_chat_id не был найден, выводим ошибку
     if alert_chat_id is None:
-        print(f"Не удалось найти chat_id для пользователя {assigned_name}.")
         check_error_logger.error("Не удалось найти chat_id для пользователя {assigned_name} %s")
     return alert_chat_id
+
+def process_ticket(ticket_data):
+    """Функция по формированию данных из тикета"""
+    status = ticket_data.get('status').get('behavior')
+    # Узнаём в работе ли ещё тикет
+    if status == "pending":
+        ticket_id = ticket_data.get('id')
+        priority_info = ticket_data.get('priority')
+        priority_name = priority_info.get('name')
+        user_info = ticket_data.get('user')
+        contact_info = user_info.get('contact_groups')
+        assigned_to = ticket_data.get('assigned_to')
+        # Проверяем, что значение для поля last_staff_reply_at существует
+        if ticket_data.get('last_staff_reply_at'):
+            reple_client_time = ticket_data.get('last_staff_reply_at')
+            time_difference = get_time_diff(reple_client_time)
+            # Проверяем, что в тикете нет ответа более 3х дней
+            if time_difference >= timedelta(days=3):
+                name_info = get_contact_name(contact_info)
+                assigned_name = get_assigned_name(assigned_to)
+                # print(f"Время последнего ответа клиента в тикете {time_difference}")
+
+                ticket_message = (f'Тикет без ответа более 3 дней: {ticket_id}\nПриоритет: {priority_name}\nНазвание клиента: {name_info}\nНазначен: {assigned_name}')
+
+                # Получаем chat_id для отправки сообщения
+                alert_chat_id = get_alert_chat_id(assigned_name)
+                
+                # Отправляем сообщение в телеграм-бот
+                # print(ticket_message)
+                # print('--'*30)
+                # print(alert_chat_id)
+                # print('**'*30)
+                send_telegram_message(alert_chat_id, ticket_message)
+                check_info_logger.info('Сотруднику: %s в чат отправлена информация о 3х дневном неотвеченном тикете: %s', assigned_name, ticket_id)
 
 def get_tickets():
     """Функция проверки тикетов, у которых нет ответа"""
     params = {
-        'status': '2',
+        #'status': '1,2,3',  # выбираем все статусы, кроме "Закрыт"
+        #'status_neq': '4',       # Исключить закрытые статусы
         'category': '1',
-        'q': 'unresponded:"true"'
+        #'q': 'behavior:pending'
+        #'q': 'unresponded:"true"'
         #'q': 'duedate:"today"'
     }
     url = API_ENDPOINT + '/tickets/?size=1&page=1'
@@ -82,10 +165,8 @@ def get_tickets():
         response = requests.get(url, auth=(API_KEY, API_SECRET), headers=HEADERS, params=params, timeout=10)
         response.raise_for_status()
     except requests.exceptions.Timeout as error_message:
-        print("Timeout error: request timed out")
         check_error_logger.error("Timeout error: request timed out: %s", error_message)
     except requests.exceptions.RequestException as error_message:
-        print(f"Error occurred: {error_message}")
         check_error_logger.error("Error occurred: %s", error_message)
         
     data_res = response.json()
@@ -101,49 +182,26 @@ def get_tickets():
                 response = requests.get(url, auth=(API_KEY, API_SECRET), headers=HEADERS, params=params, timeout=10)
                 response.raise_for_status()
             except requests.exceptions.Timeout as error_message:
-                print("Timeout error: request timed out")
                 check_error_logger.error("Timeout error: request timed out: %s", error_message)
             except requests.exceptions.RequestException as error_message:
-                print(f"Error occurred: {error_message}")
                 check_error_logger.error("Error occurred: %s", error_message)
-
             data = response.json()
             for ticket_data in data.get('data'):
-                ticket_id = ticket_data.get('id')
-                priority_info = ticket_data.get('priority')
-                priority_name = priority_info.get('name')
-                user_info = ticket_data.get('user')
-                contact_info = user_info.get('contact_groups')
-                assigned_to = ticket_data.get('assigned_to')
-                reple_client_time = ticket_data.get('last_user_reply_at')
-                time_difference = get_time_diff(reple_client_time)
-                if time_difference >= timedelta(hours=1):
-                    print(f"Время последнего ответа клиента в тикете {time_difference}")
-                    name_info = None
-                    for contact in contact_info:
-                        name_info = contact.get('name')
-                        break
-                    if isinstance(assigned_to, list):
-                        for assigned in assigned_to:
-                            assigned_name = assigned.get('name')
-                    else:
-                        if assigned_to is not None:
-                            assigned_name = assigned_to.get('name')
-                        else:
-                            assigned_name = "Нет исполнителя"
+                process_ticket(ticket_data)
+                
 
-                    ticket_message = (f'Тикет без ответа более часа: {ticket_id}\nПриоритет: {priority_name}\nНазвание клиента: {name_info}\nНазначен: {assigned_name}')
+# Создадим задачу на отправку алертов в чат
+schedule.every().day.at("10:25").do(get_tickets)
 
-                    # Получаем chat_id для отправки сообщения
-                    alert_chat_id = get_alert_chat_id(assigned_name)
-                    
-                    # Отправляем сообщение в телеграм-бот
-                    print(ticket_message)
-                    print('--'*30)
-                    print(alert_chat_id)
-                    print('**'*30)
-                    #send_telegram_message(alert_chat_id, ticket_message)
-                    check_info_logger.info('Сотруднику: %s в чат повторно отправлена информация о неотвеченном тикете: %s', assigned_name, ticket_id)
+while True:
+    # Получаем текущую дату и время
+    now = datetime.datetime.now()
+    # Проверяем, является ли текущий день недели будним днём (пн-пт)
+    if now.weekday() < 5:
+        # Запускаем отложенные задачи
+        schedule.run_pending()
+    # Ждём 1 секунду перед проверкой снова
+    time.sleep(1)
 
 if __name__ == '__main__':
     get_tickets()
