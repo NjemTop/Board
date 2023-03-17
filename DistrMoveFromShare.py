b99d1b3e9864b5a4feebfecac7d8c5d853e3b50e
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-17 15:08:05 +0300

    Add files via upload

--- DistrMoveFromShare.py
+++ DistrMoveFromShare.py
@@ -24,12 +24,9 @@ nextcloud_password = data["NEXT_CLOUD"]["PASSWORD"]
 share_path = r"//corp.boardmaps.com/data/Releases/[Server]"
 mount_point = "/mnt/windows_share"
 
-print(username)
-print(password)
-print(domain)
 # Монтируем файловую шару
 # mount_cmd = f"sudo mount -t cifs {share_path} {mount_point} -o username={username},password={password}"
-mount_cmd = f"sudo mount -t cifs {share_path} /mnt/windows_share -o username={username},password={password}"
+mount_cmd = f"sudo mount -t cifs {share_path} {mount_point} -o username={username},password={password}"
 mount_result = subprocess.run(mount_cmd, shell=True, stderr=subprocess.PIPE, text=True, check=False, timeout=30)
 
 if mount_result.returncode != 0:
