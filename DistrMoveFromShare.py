10ee98722b838b7b4625ed39c212164ac522fa6d
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-17 15:09:54 +0300

    Add files via upload

--- DistrMoveFromShare.py
+++ DistrMoveFromShare.py
@@ -26,7 +26,7 @@ mount_point = "/mnt/windows_share"
 
 # Монтируем файловую шару
 # mount_cmd = f"sudo mount -t cifs {share_path} {mount_point} -o username={username},password={password}"
-mount_cmd = f"sudo mount -t cifs {share_path} {mount_point} -o username={username},password={password}"
+mount_cmd = f"sudo mount -t cifs {share_path} {mount_point} -o username={username},password={password},domain={domain}"
 mount_result = subprocess.run(mount_cmd, shell=True, stderr=subprocess.PIPE, text=True, check=False, timeout=30)
 
 if mount_result.returncode != 0:
