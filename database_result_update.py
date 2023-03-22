05f198ea90fee21eb93968f76a4b18aaaee0da04
Эвелина <117018143+Meeline@users.noreply.github.com>
2023-03-22 23:02:28 +0300

    Add files via upload

---
+++ database_result_update.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import locale
+from datetime import datetime
+import sqlite3
+import subprocess
+from pathlib import Path
+
+setup_script = Path('Automatic_email_BS.ps1')
+version_SB = 2.62
+support_response_id = 5
+result_SB = subprocess.run(["pwsh", "-File", setup_script, str(version_SB), str(support_response_id)], stdout=subprocess.PIPE, check=True).stdout.decode('utf-8')
+
+# Запись месяца в дате по-русски
+locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+# Определяем дату рассылки = текущапя дата запуска скрипта
+today = datetime.now().date().strftime('%d %B %Y')
+
+# Составляем словарь для сбора данных:
+map = {}
+id = 0
+result = []
+# Создаем из построчного вывода общий список
+item = result_SB.split('\n')
+# Перебираем списки внутри общего списка
+for i in item:
+    # Исключаем пустой список
+    if i != '':
+        # Делим список по параметрам с пом. разделителя
+        l = i.split('|')
+        id += 1
+        map[id] = {}
+        # Наименование клиента
+        client_name = l[0]
+        # Основной контакт
+        main_contact = l[1]
+        # Копия
+        if len(l) == 3:
+            copy_contact = l[2].replace(',', ', ')
+        else:
+            copy_contact = None
+        # Добавляем данные в карту
+        map[id]["Дата_рассылки"] = today
+        map[id]["Номер_релиза"] = version_SB
+        map[id]["Наименование_клиента"] = client_name
+        map[id]["Основной_контакт"] = main_contact
+        map[id]["Копия"] = copy_contact
+        today = map[id]["Дата_рассылки"]
+        version_SB = map[id]["Номер_релиза"]
+        client_name = map[id]["Наименование_клиента"]
+        main_contact = map[id]["Основной_контакт"]
+        copy_contact = map[id]["Копия"]
+        result.append([today, version_SB, client_name, main_contact, copy_contact])
+    continue
+
+# Подключаемся к БД
+conn = sqlite3.connect('DataBase/database.db')
+c = conn.cursor()
+# Создаем БД
+c.execute('CREATE TABLE IF NOT EXISTS info (Дата_рассылки date, Номер_релиза number, Наименование_клиента text, Основной_контакт text, Копия text)')
+conn.commit()
+# Наполняем БД
+df = pd.DataFrame(result, columns = ["Дата_рассылки", "Номер_релиза", "Наименование_клиента", "Основной_контакт", "Копия"])
+df.to_sql('info', conn, if_exists='append', index = False)
