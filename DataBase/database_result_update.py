773f965c232bbcc9dd4a50af443f4c7fe4d687cc
Meeline <mokrova.ev@inbox.ru>
2023-03-22 23:14:54 +0300

    добавлена функция в бот вызова БД

--- DataBase/database_result_update.py
+++ DataBase/database_result_update.py
@@ -3,7 +3,7 @@ import locale
 from datetime import datetime
 import sqlite3
 
-def upload_db_result(version_SB, result_SB):
+def upload_db_result(version_number, result):
     # Запись месяца в дате по-русски
     locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
     # Определяем дату рассылки = текущапя дата запуска скрипта
@@ -12,9 +12,9 @@ def upload_db_result(version_SB, result_SB):
     # Составляем словарь для сбора данных:
     map = {}
     id = 0
-    result = []
+    result_db = []
     # Создаем из построчного вывода общий список
-    item = result_SB.split('\n')
+    item = result.split('\n')
     # Перебираем списки внутри общего списка
     for i in item:
         # Исключаем пустой список
@@ -34,16 +34,16 @@ def upload_db_result(version_SB, result_SB):
                 copy_contact = None
             # Добавляем данные в карту
             map[id]["Дата_рассылки"] = today
-            map[id]["Номер_релиза"] = version_SB
+            map[id]["Номер_релиза"] = version_number
             map[id]["Наименование_клиента"] = client_name
             map[id]["Основной_контакт"] = main_contact
             map[id]["Копия"] = copy_contact
             today = map[id]["Дата_рассылки"]
-            version_SB = map[id]["Номер_релиза"]
+            version_number = map[id]["Номер_релиза"]
             client_name = map[id]["Наименование_клиента"]
             main_contact = map[id]["Основной_контакт"]
             copy_contact = map[id]["Копия"]
-            result.append([today, version_SB, client_name, main_contact, copy_contact])
+            result_db.append([today, version_number, client_name, main_contact, copy_contact])
         continue
 
     # Подключаемся к БД
@@ -53,5 +53,5 @@ def upload_db_result(version_SB, result_SB):
     c.execute('CREATE TABLE IF NOT EXISTS info (Дата_рассылки date, Номер_релиза number, Наименование_клиента text, Основной_контакт text, Копия text)')
     conn.commit()
     # Наполняем БД
-    df = pd.DataFrame(result, columns = ["Дата_рассылки", "Номер_релиза", "Наименование_клиента", "Основной_контакт", "Копия"])
+    df = pd.DataFrame(result_db, columns = ["Дата_рассылки", "Номер_релиза", "Наименование_клиента", "Основной_контакт", "Копия"])
     df.to_sql('info', conn, if_exists='append', index = False)
