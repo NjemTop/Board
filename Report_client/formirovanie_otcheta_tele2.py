22a19430d1d2ee894436cececceeabde2f4d0582
Meeline <mokrova.ev@inbox.ru>
2023-03-25 12:20:06 +0300

    local

--- Report_client/formirovanie_otcheta_tele2.py
+++ Report_client/formirovanie_otcheta_tele2.py
@@ -6,7 +6,7 @@ import requests
 def auth_hf_tele2(client_report_id):
     ############################## ШАБЛОН ДЛЯ ТЕЛЕ2
     ## Создаем файл и делаем русскую локализацию для даты
-    # locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+    locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
     ## Авторизация в HappyFox и нахождение кол-ва страниц по url первой страницы
     auth = ('45357d176a5f4e25b740aebae58f189c','3b9e5c6cc6f34802ad5ae82bafdab3bd')
     headers = {'Content-Type': 'application/json'}
