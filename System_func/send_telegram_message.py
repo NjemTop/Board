e4914d3034338fb1e9798c4ac5c15472a114dcce
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-26 21:27:14 +0300

    +

--- System_func/send_telegram_message.py
+++ System_func/send_telegram_message.py
@@ -1,5 +1,25 @@
 import requests
 import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='web_server.log', filemode='w',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M')
+# Создание объекта логгера для ошибок и критических событий
+web_error_logger = logging.getLogger('WebError')
+web_error_logger.setLevel(logging.ERROR)
+web_error_handler = logging.FileHandler('./logs/web-errors.log')
+web_error_handler.setLevel(logging.ERROR)
+web_error_handler.setFormatter(formatter)
+web_error_logger.addHandler(web_error_handler)
+
+# Создание объекта логгера для информационных сообщений
+web_info_logger = logging.getLogger('WebInfo')
+web_info_logger.setLevel(logging.INFO)
+web_info_handler = logging.FileHandler('./logs/web-info.log')
+web_info_handler.setLevel(logging.INFO)
+web_info_handler.setFormatter(formatter)
+web_info_logger.addHandler(web_info_handler)
 
 # Указываем путь к файлу с данными
 CONFIG_FILE = "Main.config"
@@ -19,4 +39,6 @@ class Alert():
         headers_server = {'Content-type': 'application/json'}
         data = {'chat_id': alert_chat_id, 'text': f'{alert_text}'}
         response = requests.post(url, headers=headers_server, data=json.dumps(data), timeout=30)
-        print(response)
+        # Добавляем логгирование для отладки
+        web_info_logger.info("send_telegram_message function was called with parameters: alert_chat_id=%s, alert_text=%s", alert_chat_id, alert_text)
+        web_info_logger.info("Response from Telegram API: %s", response)
