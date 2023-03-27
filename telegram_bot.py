c644164b5cf2e39926b4c840d7e0dea3e99e1f87
Meeline <mokrova.ev@inbox.ru>
2023-03-28 00:48:27 +0300

    524

--- telegram_bot.py
+++ telegram_bot.py
@@ -638,14 +638,14 @@ def inline_button_else_tickets(call):
     elif call.data == "cancel_else_tickets":
         user_states[call.message.chat.id] = "canceled"
         # Возвращаемся на уровень выше
-        button_else_tickets = ButtonElseTickets.get_info_one_ticket_stat()
+        button_else_tickets = ButtonElseTickets.get_info_else_tickets()
         bot.edit_message_text('Выберите раздел:', call.message.chat.id, call.message.message_id,reply_markup=button_else_tickets)
 
 @bot.chosen_inline_handler(func=lambda get_number_else_ticket: True)
 def get_number_else_ticket(result_number_else_ticket):
     user_state = user_states.get(result_number_else_ticket.chat.id)
     if user_state == "waiting_for_client_name":
-        button_else_tickets = ButtonElseTickets.button_else_tickets()
+        button_else_tickets = ButtonElseTickets.get_info_one_ticket_stat()
         bot.send_message(result_number_else_ticket.from_user.id, text="Скоро здесь будет инфо тикета.", reply_markup=button_else_tickets).text.lower()
     else:
         pass
