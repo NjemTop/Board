d6e1784a78457dc3839287685ff3962d51071248
Meeline <mokrova.ev@inbox.ru>
2023-03-28 00:41:58 +0300

    452

--- ButtonClasses/button_else_tickets.py
+++ ButtonClasses/button_else_tickets.py
@@ -1,14 +1,14 @@
 from telebot import types
 
 class ButtonElseTickets():
-    def get_info_else_tickets(call):
+    def get_info_else_tickets():
         button_else_tickets = types.InlineKeyboardButton
         button_else_tickets_stat = types.InlineKeyboardMarkup(text= 'Общая статистика', callback_data='button_else_tickets_stat')
         button_one_ticket_stat = types.InlineKeyboardMarkup(text= 'Просмотр данных отдельного тикета', callback_data='button_one_ticket_stat')
         main_menu = types.InlineKeyboardButton(text= 'Главное меню', callback_data='mainmenu')
         button_else_tickets.add(button_else_tickets_stat, button_one_ticket_stat, main_menu, row_width=1)
         return button_else_tickets
-    def get_info_else_tickets_stat(call):    
+    def get_info_else_tickets_stat():    
         button_else_tickets_stat = types.InlineKeyboardButton
         back_from_button_else_tickets_stat = types.InlineKeyboardButton(text= 'Назад', callback_data='button_else_tickets')
         main_menu = types.InlineKeyboardButton(text= 'Главное меню', callback_data='mainmenu')
@@ -18,7 +18,7 @@ class ButtonElseTickets():
         without_support_answer = '3'
         without_client_ansrew = '1'
         return button_else_tickets_stat, all_ticket_count, new_ticket, without_support_answer, without_client_ansrew
-    def get_info_one_ticket_stat(call):    
+    def get_info_one_ticket_stat():    
         button_one_ticket_stat = types.InlineKeyboardButton
         button_one_ticket_cancel = types.InlineKeyboardButton(text= 'Отмена', callback_data='cancel_else_tickets')
         button_one_ticket_stat.add(button_one_ticket_cancel, row_width=1)
