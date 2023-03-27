8fcee2c611a0d1947caeebaa2cbd74d97f63a803
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-27 11:09:06 +0300

    ++

--- System_func/send_telegram_message.py
+++ System_func/send_telegram_message.py
@@ -40,5 +40,4 @@ class Alert():
         data = {'chat_id': alert_chat_id, 'text': f'{alert_text}'}
         response = requests.post(url, headers=headers_server, data=json.dumps(data), timeout=30)
         # Добавляем логгирование для отладки
-        web_info_logger.info("send_telegram_message function was called with parameters: alert_chat_id=%s, alert_text=%s", alert_chat_id, alert_text)
         web_info_logger.info("Response from Telegram API: %s", response)
