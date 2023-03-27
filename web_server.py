9db78dbae8662c9eb9904999c05c41ef68bcc90b
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-27 18:14:10 +0300

    +++

--- web_server.py
+++ web_server.py
@@ -521,7 +521,7 @@ def get_app():
                 'Основной_контакт': row[3],
                 'Копия': row[4]
             })
-        return render_template('.templates/data_release.html', data=data)
+        return render_template('./templates/data_release.html', data=data)
         
     return app
 
