cfb8f9521f4c8b870e20504e449be9ae9a673be6
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-28 14:06:39 +0300

    ++

--- DataBase/manage.py
+++ DataBase/manage.py
@@ -3,27 +3,32 @@ from model_class import BaseModel, ClientsInfo, Release_info, conn
 
 # Определяем команду для миграции базы данных
 def migrate():
-    # Обход всех моделей
-    for model in [ClientsInfo, Release_info]:
-        # Проверяем наличие таблицы в БД
-        if not model.table_exists():
-            # Если таблицы нет, то создаем ее
-            with conn:
-                conn.create_table(model)
-
-        else:
-            # Если таблица существует, проверяем ее поля (столбцы)
-            current_columns = set(model.columns.keys())
-            expected_columns = set([field.name for field in model._meta.fields])
-
-            # Если поля отсутствуют, добавляем их
-            new_columns = expected_columns - current_columns
-            for column in new_columns:
+    try:
+        # Обход всех моделей
+        for model in [ClientsInfo, Release_info]:
+            # Проверяем наличие таблицы в БД
+            if not model.table_exists():
+                # Если таблицы нет, то создаем ее
                 with conn:
-                    conn.execute_sql(f"ALTER TABLE {model._meta.table_name} ADD COLUMN {column} TEXT")
+                    conn.create_table(model)
 
-            # Если поля неожиданно присутствуют, удаляем их
-            old_columns = current_columns - expected_columns
-            for column in old_columns:
-                with conn:
-                    conn.execute_sql(f"ALTER TABLE {model._meta.table_name} DROP COLUMN {column}")
+            else:
+                # Если таблица существует, проверяем ее поля (столбцы)
+                current_columns = set(model.columns.keys())
+                expected_columns = set([field.name for field in model._meta.fields])
+
+                # Если поля отсутствуют, добавляем их
+                new_columns = expected_columns - current_columns
+                for column in new_columns:
+                    with conn:
+                        conn.execute_sql(f"ALTER TABLE {model._meta.table_name} ADD COLUMN {column} TEXT")
+
+                # Если поля неожиданно присутствуют, удаляем их
+                old_columns = current_columns - expected_columns
+                for column in old_columns:
+                    with conn:
+                        conn.execute_sql(f"ALTER TABLE {model._meta.table_name} DROP COLUMN {column}")
+
+        print("Tables created successfully")
+    except Exception as error_message:
+            print("Error during table creation:", error_message)
