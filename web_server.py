b664cb505bc9b05264868a85debe4b517f0430f5
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-28 23:54:19 +0300

    +

--- web_server.py
+++ web_server.py
@@ -514,13 +514,13 @@ def post_BM_Info_onClient_api():
         # Сохраняем данные в базе данных
         with conn.atomic():
             # Проверяем наличие существующего клиента с тем же именем
-            existing_client = BMInfo_onClient.get_or_none(BMInfo_onClient.client_name == data['Название_клиента'])
+            existing_client = BMInfo_onClient.get_or_none(BMInfo_onClient.client_name == data['client_name'])
 
             if existing_client is None:
                 client_info.save()
             else:
-                print(f"Клиент с именем {data['Название_клиента']} уже существует. Пропускаем...")
-                return f"Клиент с именем {data['Название_клиента']} уже существует. Пропускаем..."
+                print(f"Клиент с именем {data['client_name']} уже существует. Пропускаем...")
+                return f"Клиент с именем {data['client_name']} уже существует. Пропускаем..."
 
         return 'Data successfully saved to the database!'
 
