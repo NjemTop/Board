cfcf650ebe8e74ecea909b596072d67a46708851
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-30 19:29:44 +0300

    log other

--- HappyFox/schedule_ticket_check.py
+++ HappyFox/schedule_ticket_check.py
@@ -4,23 +4,11 @@ import datetime
 import time
 import os
 from HappyFox.happyfox_class import HappyFoxConnector
+from log_config import setup_logger, get_abs_log_path
 
-# Создание объекта логгера для ошибок и критических событий
-check_error_logger = logging.getLogger('Check_Ticket_Error')
-check_error_logger.setLevel(logging.ERROR)
-check_error_handler = logging.FileHandler('../logs/check_ticket-errors.log')
-check_error_handler.setLevel(logging.ERROR)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M')
-check_error_handler.setFormatter(formatter)
-check_error_logger.addHandler(check_error_handler)
-
-# Создание объекта логгера для информационных сообщений
-check_info_logger = logging.getLogger('Check_Ticket_Info')
-check_info_logger.setLevel(logging.INFO)
-check_info_handler = logging.FileHandler('../logs/check_ticket-info.log')
-check_info_handler.setLevel(logging.INFO)
-check_info_handler.setFormatter(formatter)
-check_info_logger.addHandler(check_info_handler)
+# Указываем настройки логов для нашего файла с классами
+check_error_logger = setup_logger('Check_Ticket_Error', get_abs_log_path('check_ticket-errors.log'), logging.ERROR)
+check_info_logger = setup_logger('Check_Ticket_Info', get_abs_log_path('check_ticket-info.log'), logging.INFO)
 
 # Получаем абсолютный путь до файла schedule_ticket_check.py
 current_file_path = os.path.abspath(os.path.dirname(__file__))
--- HappyFox/ticket_utils.py
+++ HappyFox/ticket_utils.py
@@ -1,23 +1,11 @@
 import datetime
 import logging
 import xml.etree.ElementTree as ET
+from log_config import setup_logger, get_abs_log_path
 
-# Создание объекта логгера для ошибок и критических событий
-check_error_logger = logging.getLogger('Check_Ticket_Error')
-check_error_logger.setLevel(logging.ERROR)
-check_error_handler = logging.FileHandler('./logs/check_ticket-errors.log')
-check_error_handler.setLevel(logging.ERROR)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M')
-check_error_handler.setFormatter(formatter)
-check_error_logger.addHandler(check_error_handler)
-
-# Создание объекта логгера для информационных сообщений
-check_info_logger = logging.getLogger('Check_Ticket_Info')
-check_info_logger.setLevel(logging.INFO)
-check_info_handler = logging.FileHandler('./logs/check_ticket-info.log')
-check_info_handler.setLevel(logging.INFO)
-check_info_handler.setFormatter(formatter)
-check_info_logger.addHandler(check_info_handler)
+# Указываем настройки логов для нашего файла с классами
+check_error_logger = setup_logger('Check_Ticket_Error', get_abs_log_path('check_ticket-errors.log'), logging.ERROR)
+check_info_logger = setup_logger('Check_Ticket_Info', get_abs_log_path('check_ticket-info.log'), logging.INFO)
 
 class TicketUtils:
     @staticmethod
