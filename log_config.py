563664d6dede871cdca86877f976b7108c3bd02a
Олег <106114697+NjemTop@users.noreply.github.com>
2023-03-30 19:13:30 +0300

    logs cd

--- log_config.py
+++ log_config.py
@@ -1,9 +1,14 @@
 import logging
 import os
 
-def get_abs_log_path(log_file):
-    """Функция для определения места записи логов"""
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'logs', log_file))
+def get_abs_log_path(log_filename):
+    # Получаем абсолютный путь до файла с настройками логгера (log_config.py)
+    log_config_path = os.path.abspath(os.path.dirname(__file__))
+    # Получаем абсолютный путь до корневой папки проекта
+    project_root = os.path.dirname(log_config_path)
+    # Формируем абсолютный путь до папки logs и файла с логами
+    log_file_path = os.path.join(project_root, 'logs', log_filename)
+    return log_file_path
 
 def setup_logger(name, log_file, level=logging.INFO, formatter=None):
     if formatter is None:
