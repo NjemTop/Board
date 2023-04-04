659b491e019ad3d7687ad806a819b12f764af0c0
Олег <106114697+NjemTop@users.noreply.github.com>
2023-04-04 15:54:50 +0300

    ++

--- web_server.py
+++ web_server.py
@@ -1335,19 +1335,18 @@ def patch_integration_api(client_id):
     return jsonify({'message': 'Данные интеграции успешно обновлены'}), 200
 
 def get_tech_account_api(client_id):
-    # Получаем все записи с указанным tech_account_id
     try:
         with conn:
-            client = TechAccount.select().where(TechAccount.tech_account_id == tech_account_id)
+            client = ClientsCard.get(ClientsCard.client_id == client_id)
     except DoesNotExist:
-        return jsonify({'error': 'Нет данных о технических аккаунтах'}), 404
+        return jsonify({'error': f'Клиент с ID {client_id} не найден'}), 404
 
     tech_account_id = client.tech_account
 
-    # Получаем все записи с указанным client_id
+    # Получаем все записи с указанным tech_account_id
     try:
         with conn:
-            tech_accounts = TechAccount.select().where(TechAccount.client_id == tech_account_id)
+            tech_accounts = TechAccount.select().where(TechAccount.tech_account_id == tech_account_id)
     except DoesNotExist:
         return jsonify({'error': 'Нет данных о технических аккаунтах'}), 404
 
