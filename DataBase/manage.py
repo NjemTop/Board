d9d4e2ff1190143801ae8b792b317de18da0fa6b
Олег <106114697+NjemTop@users.noreply.github.com>
2023-04-04 15:42:48 +0300

    +++

--- DataBase/manage.py
+++ DataBase/manage.py
@@ -10,7 +10,7 @@ def migrate():
             if not model.table_exists():
                 with conn:
                     conn.create_tables([model])
-                    print(f"Таблица {model} создана")
+                    print(f"Таблица {model._meta.table_name} создана")
             else:
                 # Получаем столбцы таблицы и модели
                 table_columns = conn.get_columns(model._meta.table_name)
@@ -18,7 +18,7 @@ def migrate():
                 model_column_names = set(model().columns.keys())
 
                 if table_column_names != model_column_names:
-                    print(f"Начат процесс миграции строк таблицы {model}")
+                    print(f"Начат процесс миграции строк таблицы {model._meta.table_name}")
                     # Создаем новую таблицу с обновленными столбцами
                     new_table_name = model._meta.table_name + '_new'
 
@@ -29,17 +29,17 @@ def migrate():
 
                     with conn:
                         conn.create_tables([TempModel])
-                        print(f"Новая таблица {model} создана")
+                        print(f"Новая таблица {model._meta.table_name} создана")
 
                     # Копируем данные из старой таблицы в новую
                     common_columns = table_column_names.intersection(model_column_names)
                     common_columns_str = ', '.join(common_columns)
-                    print(f"Старые столбцы: {table_column_names}, для таблицы {model}.")
-                    print(f"Новые столбцы: {model_column_names}, для таблицы {model}.")
+                    print(f"Старые столбцы: {table_column_names}, для таблицы {model._meta.table_name}.")
+                    print(f"Новые столбцы: {model_column_names}, для таблицы {model._meta.table_name}.")
                     query = f"INSERT INTO {new_table_name} ({common_columns_str}) SELECT {common_columns_str} FROM {model._meta.table_name};"
                     with conn:
                         conn.execute_sql(query)
-                        print(f"Копирование таблицы {model} завершено")
+                        print(f"Копирование таблицы {model._meta.table_name} завершено")
 
                     # Удаляем старую таблицу
                     with conn:
@@ -49,7 +49,7 @@ def migrate():
                     # Переименовываем новую таблицу
                     with conn:
                         conn.execute_sql(f"ALTER TABLE {new_table_name} RENAME TO {model._meta.table_name}")
-                        print(f"Переименование таблицы {new_table_name} в {model} завершено")
+                        print(f"Переименование таблицы {new_table_name} в {model._meta.table_name} завершено")
 
         print("Tables migrated successfully")
     except Exception as error_masage:
