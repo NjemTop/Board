997c0da4936206aa508c1d4905ddd1b31eca2de0
Олег <106114697+NjemTop@users.noreply.github.com>
2023-04-07 16:18:02 +0300

    eddit path

--- telegram_bot.py
+++ telegram_bot.py
@@ -642,7 +642,9 @@ def answer_start_end_date_psb(answer_id_psb):
         end_date = two_date[1]
         bot.send_message(answer_id_psb.from_user.id, text='Пожалуйста, ожидайте. По завершении процесса, в чат будет отправлен файл отчета.')
         contact_group_id = 21
-        template_path = Path(__file__) / 'templates' / 'Temp_report_PSB_.docx'
+        #template_path = Path(__file__).parent / 'templates' / 'Temp_report_PSB_.docx'
+        template_path = os.path.join(os.getcwd(), 'templates', 'Temp_report_PSB_.docx')
+        print(os.listdir(os.path.join(os.getcwd(), 'templates')))
         if not os.path.exists(template_path):
             bot.send_message(answer_id_psb.from_user.id, text=f"Ошибка: файл шаблона не найден по пути {template_path}")
             return
