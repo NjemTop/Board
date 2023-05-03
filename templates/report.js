74c04101b23b933f67dd28061157d07f5b2f67fb
Олег <106114697+NjemTop@users.noreply.github.com>
2023-05-04 01:16:57 +0300

    изменил функцию чтобы быыл фильтр

--- templates/report.js
+++ templates/report.js
@@ -1,11 +1,7 @@
-
-// Получить данные отчета из Flask-приложения
 const reportData = {{ report_data|tojson|safe }};
 
-// Получить уникальные даты отчетов
 const uniqueReportDates = [...new Set(reportData.map(entry => entry.report_date))];
 
-// Добавить уникальные даты отчетов в выпадающий список
 const reportDateSelect = document.getElementById('report-date-select');
 uniqueReportDates.forEach(date => {
     const option = document.createElement('option');
@@ -14,19 +10,17 @@ uniqueReportDates.forEach(date => {
     reportDateSelect.appendChild(option);
 });
 
-// Функция для отображения отфильтрованных данных
 function displayData(selectedDate) {
     const tableBody = document.querySelector('table tbody');
     tableBody.innerHTML = '';
-    
+
     const filteredData = selectedDate === 'all'
         ? reportData
         : reportData.filter(entry => entry.report_date === selectedDate);
-        
+
     filteredData.forEach(entry => {
         const row = tableBody.insertRow();
-        
-        // Добавьте ячейки таблицы для каждого столбца, кроме 'report_date'
+
         for (const key in entry) {
             if (key !== 'report_date') {
                 const cell = row.insertCell();
@@ -36,10 +30,8 @@ function displayData(selectedDate) {
     });
 }
 
-// Обновить данные таблицы при изменении значения выпадающего списка
 reportDateSelect.addEventListener('change', () => {
     displayData(reportDateSelect.value);
 });
 
-// Отобразить все данные по умолчанию
 displayData('all');
