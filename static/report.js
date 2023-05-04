51a32783dba1ea65f96a461787bcad18ff4471b0
Олег <106114697+NjemTop@users.noreply.github.com>
2023-05-04 17:38:08 +0300

    +

--- static/report.js
+++ static/report.js
@@ -57,6 +57,20 @@ function displayData(data) {
     updateChartData(data);
 }
 
+function updateChartData(reportDate) {
+    const filteredData = reportDate === 'all' ? chartData : chartData.filter(entry => entry.report_date === reportDate);
+    let causeCount = {};
+
+    filteredData.forEach(entry => {
+        const cause = entry.cause;
+        causeCount[cause] = (causeCount[cause] || 0) + 1;
+    });
+
+    chart.data.labels = Object.keys(causeCount);
+    chart.data.datasets[0].data = Object.values(causeCount);
+    chart.update();
+}
+
 // Изначально показываем данные за текущую неделю
 const thisMonday = moment().startOf('week').add(1, 'days'); // Начало текущей недели, понедельник
 const today = moment(); // Сегодняшний день
