cc85888a41bc0b13b5ed57e5e431c4582b697805
Олег <106114697+NjemTop@users.noreply.github.com>
2023-05-04 17:05:41 +0300

    вернул как было без Vue

--- static/report.js
+++ static/report.js
@@ -1,86 +1,48 @@
 const reportDataTag = document.getElementById('report-data-tag');
 const reportData = JSON.parse(reportDataTag.textContent);
+const reportDateSelect = document.getElementById('report-date-select');
 
-Vue.component('date-picker', Vue2DatePicker);
+const uniqueDates = Array.from(new Set(reportData.map(entry => entry.report_date))).sort();
 
-const app = new Vue({
-    el: '#app',
-    data: {
-        reportData: reportData,
-        dateRange: '',
-        selectedMonth: 'all',
-        uniqueMonths: Array.from(new Set(reportData.map(entry => {
-            const [day, month, year] = entry.report_date.split('-');
-            return `${month}-${year}`;
-        }))).sort(),
-    },
-    methods: {
-        displayData(dateRange) {
-            // сброс выбранного месяца
-            this.selectedMonth = 'all';
-    
-            const tableBody = document.querySelector('#report-table tbody');
-            tableBody.innerHTML = '';
-    
-            let filteredData;
-            if (dateRange === '') {
-                filteredData = reportData;
-            } else {
-                const [startDate, endDate] = dateRange.split(' ~ ').map(date => new Date(date.split('-').reverse().join('-')));
-                filteredData = reportData.filter(entry => {
-                    const entryDate = new Date(entry.report_date.split('-').reverse().join('-'));
-                    return entryDate >= startDate && entryDate <= endDate;
-                });
-            }
-    
-            this.updateTable(filteredData);
-            this.updateChartData(filteredData);
-        },
-        displayDataByMonth(selectedMonth) {
-            // сброс выбранного диапазона дат
-            this.dateRange = '';
-    
-            const tableBody = document.querySelector('#report-table tbody');
-            tableBody.innerHTML = '';
-    
-            let filteredData;
-            if (selectedMonth === 'all') {
-                filteredData = reportData;
-            } else {
-                filteredData = reportData.filter(entry => {
-                    const [day, month, year] = entry.report_date.split('-');
-                    return `${month}-${year}` === selectedMonth;
-                });
-            }
-    
-            this.updateTable(filteredData);
-            this.updateChartData(filteredData);
-        },
-        updateTable(filteredData) {
-            const tableBody = document.querySelector('#report-table tbody');
-            tableBody.innerHTML = '';
-        
-            filteredData.forEach(entry => {
-                const row = document.createElement('tr');
-                row.innerHTML = `
-                    <td>${entry.ticket_id}</td>
-                    <td>${entry.subject}</td>
-                    <td>${entry.creation_date}</td>
-                    <td>${entry.status}</td>
-                    <td>${entry.client_name}</td>
-                    <td>${entry.priority}</td>
-                    <td>${entry.assignee_name}</td>
-                    <td>${entry.updated_at}</td>
-                    <td>${entry.last_reply_at}</td>
-                    <td>${entry.sla}</td>
-                    <td>${entry.sla_time}</td>
-                    <td>${entry.response_time}</td>
-                    <td>${entry.cause}</td>
-                    <td>${entry.module_boardmaps}</td>
-                    <td>${entry.staff_message}</td>
-                `;
-                tableBody.appendChild(row);
-            });
-        },
-    },
+uniqueDates.forEach(date => {
+    const option = document.createElement('option');
+    option.value = date;
+    option.text = date;
+    reportDateSelect.appendChild(option);
 });
+
+function displayData(reportDate) {
+    const tableBody = document.querySelector('#report-table tbody');
+    tableBody.innerHTML = '';
+
+    const filteredData = reportDate === 'all' ? reportData : reportData.filter(entry => entry.report_date === reportDate);
+
+    filteredData.forEach(entry => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${entry.ticket_id}</td>
+            <td>${entry.subject}</td>
+            <td>${entry.creation_date}</td>
+            <td>${entry.status}</td>
+            <td>${entry.client_name}</td>
+            <td>${entry.priority}</td>
+            <td>${entry.assignee_name}</td>
+            <td>${entry.updated_at}</td>
+            <td>${entry.last_reply_at}</td>
+            <td>${entry.sla}</td>
+            <td>${entry.sla_time}</td>
+            <td>${entry.response_time}</td>
+            <td>${entry.cause}</td>
+            <td>${entry.module_boardmaps}</td>
+            <td>${entry.staff_message}</td>
+        `;
+        tableBody.appendChild(row);
+    });
+}
+
+reportDateSelect.addEventListener('change', () => {
+    const selectedDate = reportDateSelect.value;
+    displayData(selectedDate);
+});
+
+displayData('all');
