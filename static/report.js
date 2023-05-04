dd33a2b349f97be90aa6dcb1b99ca77c07a06cd2
Олег <106114697+NjemTop@users.noreply.github.com>
2023-05-04 12:04:06 +0300

    пробуем

--- static/report.js
+++ static/report.js
@@ -1,23 +1,38 @@
 const reportDataTag = document.getElementById('report-data-tag');
 const reportData = JSON.parse(reportDataTag.textContent);
 
-const uniqueReportDates = [...new Set(reportData.map(entry => entry.creation_date))];
+function displayData(reportDate) {
+    const tableBody = document.querySelector('#report-table tbody');
+    tableBody.innerHTML = '';
 
-const reportDateSelect = document.getElementById('report-date-select');
-uniqueReportDates.forEach(date => {
-    if (date) {
-        const option = document.createElement('option');
-        option.value = date;
-        option.textContent = date;
-        reportDateSelect.appendChild(option);
-    }
-});
+    const filteredData = reportDate === 'all' ? reportData : reportData.filter(entry => entry.creation_date === reportDate);
+
+    filteredData.forEach(entry => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${entry.ticket_id}</td>
+            <td>${entry.subject}</td>
+            <td>${entry.creation_date}</td>
+            <td>${entry.status}</td>
+            <td>${entry.client_name}</td>
+            <td>${entry.priority}</td>
+            <td>${entry.assignee_name}</td>
+            <td>${entry.updated_at}</td>
+            <td>${entry.last_reply_at}</td>
+            <td>${entry.sla}</td>
+            <td>${entry.sla_time}</td>
+            <td>${entry.response_time}</td>
+            <td>${entry.cause}</td>
+            <td>${entry.module_boardmaps}</td>
+            <td>${entry.staff_message}</td>
+        `;
+        tableBody.appendChild(row);
+    });
+}
 
 reportDateSelect.addEventListener('change', () => {
     const selectedDate = reportDateSelect.value;
-    if (selectedDate) {
-        window.location.href = `/report_tickets?report_date=${selectedDate}`;
-    } else {
-        window.location.href = `/report_tickets`;
-    }
+    displayData(selectedDate);
 });
+
+displayData('all');
